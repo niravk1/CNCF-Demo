da847ad654419170f711fa430fd2b3a65106034d
Nirav Kamdar <nirav.c.kamdar@oracle.com>
2019-05-04 15:54:10 +1000

    Updated install-app.sh

--- Istio/install-app.sh
+++ Istio/install-app.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-set -x
-source ./vars.sh
-
 if [ -d microservices-demo ]; then
   cd microservices-demo
 else
@@ -15,19 +12,8 @@ if [ ! -f ../sock-shop-patch.applied ]; then
   touch ../sock-shop-patch.applied
 fi
 
-kubectl config use-context $context
 kubectl apply -f deploy/kubernetes/manifests/sock-shop-ns.yaml
 kubectl apply -f ../istio.patch
 kubectl label namespace sock-shop istio-injection=enabled
 kubectl apply -f deploy/kubernetes/complete-demo.yaml
 kubectl apply -f ../gateway.yml
-kubectl apply -f ../cluster-1-service-entries.yml
-
-
-kubectl config use-context $context2
-kubectl apply -f deploy/kubernetes/manifests/sock-shop-ns.yaml
-kubectl apply -f ../istio.patch
-kubectl label namespace sock-shop istio-injection=enabled
-kubectl apply -f deploy/kubernetes/complete-demo.yaml
-kubectl apply -f ../gateway.yml
-kubectl apply -f ../cluster-2-service-entries.yml

6e8e1018210b201eb634d0e50615fb9363071947
Nirav Kamdar <nirav.c.kamdar@oracle.com>
2019-05-09 21:04:48 +1000

    Added files required for SockShop demo with Istio

--- Istio/deploy_sock-shop.sh
+++ Istio/deploy_sock-shop.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd /home/kube/CNCF-Demo/Istio
 kubectl apply -f microservices-demo/deploy/kubernetes/complete-demo.yaml
-kubectl apply -f gateway.yml
+#kubectl apply -f gateway.yml
 kubectl apply -f DestinationRule-All.yaml
 kubectl apply -f VirtualService-All.yaml
-
